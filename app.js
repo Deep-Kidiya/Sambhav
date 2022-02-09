895d7709b7bb3818e33a377209eed3141e558d70
Deep Kidiya <95274924+Deep-Kidiya@users.noreply.github.com>
2022-02-09 10:28:29 +0530

    z

---
+++ app.js
@@ -0,0 +1,8 @@
+var wrapperMenu = document.querySelector('.wrapper-menu');
+
+wrapperMenu.addEventListener('click', function(){
+  wrapperMenu.classList.toggle('open');  
+})
+
+
+// ==========================================================================
